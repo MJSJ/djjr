1985885324def39d6c120dd29b911692f0c92b60
yursile <289181450@qq.com>
2016-06-27 22:47:56 +0800

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,16 @@ $(".toggle_nav").click(function(){
 });
 
 
-$(".p_detail").delegate("a","click",function(){
-	// $(this).
+$(".p_detail").delegate("a","click",function(e){
+	
+	var id = $(this).data("id");
+	var html = template('snapshotTPL', snapshots[id]);
+    $(".snapshot_wrapper figure").html(html);
+	e.preventDefault();
+	if(!$(".snapshot").hasClass("animated"))
+		$(".snapshot").addClass("animated bounceInUp");
+})
+
+$(".snapshot_toggle").on("click",function(){
+	$(".snapshot").removeClass("bounceInUp animated");
 })
